03f4a2dc83154c192f8541bc89b96c8d72ee51c8
Afzal Diwan <afzalhusen3@gmail.com>
2023-04-17 13:56:04 +0530

    added cors package

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import cors from "cors"
 
 const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+app.use(cors())
 
 app.get("/download/:filename", (req, res) => {
   const filePath = __dirname + "/public/assets/" + req.params.filename;
